a2fcc3b6b41b1d03f6c88f6cb78f845efda8bea7
DavidDag <david.daghelian@ibm.com>
2019-08-12 10:29:15 -0400

    Fixed very small bug which causes a open ( error

--- DemoMain.py
+++ DemoMain.py
@@ -7,7 +7,7 @@ def invalid_parameters(i):
 
 params = sys.argv[1:]
 valid_params = ["-gen", "-game", "-name"]
-params_out = ["267", "machine", "'Super Mario Bros (Machine Learned).nes'"]
+params_out = ["267", "machine", "Super Mario Bros (Machine Learned).nes"]
 default_rom = params_out[0]
 completed_params = []
 
@@ -28,7 +28,7 @@ if len(params) % 2 == 0:
     load = True
 else:
     invalid_parameters(3)
-   
+
 if params_out[valid_params.index("-game")].lower() == "create":
     os.system("cd 'Map Creator/'; python MapCreatorMain.py " + params_out[valid_params.index("-name")] + " " + params_out[valid_params.index("-gen")])
 elif params_out[valid_params.index("-game")].lower() == "machine":
